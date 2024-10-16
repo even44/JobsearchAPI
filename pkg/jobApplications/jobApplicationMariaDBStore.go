72d479ffbe557ae81a040547bf9e46e5b8c4ec2e
even44 <even.andreas.ingeberg@ebnett.no>
2024-10-16 17:33:26 +0200

    MariaDBStore now forces id=0 when adding new JobApplication

--- pkg/jobApplications/jobApplicationMariaDBStore.go
+++ pkg/jobApplications/jobApplicationMariaDBStore.go
@@ -11,13 +11,12 @@ type MariaDBStore struct {
 	db *gorm.DB
 }
 
-func NewMariaDBStore() *MariaDBStore{
+func NewMariaDBStore() *MariaDBStore {
 	db, err := connectToMariaDB()
 	if err != nil {
 		log.Fatal(err)
 	}
-	
-	
+
 	err = db.AutoMigrate(&JobApplication{})
 	if err != nil {
 		log.Fatal(err)
@@ -29,22 +28,22 @@ func NewMariaDBStore() *MariaDBStore{
 }
 
 func connectToMariaDB() (*gorm.DB, error) {
-    dsn := "root:superroot@tcp(db:3306)/jobsearchdb?charset=utf8mb4&parseTime=True&loc=Local"
-    db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-    if err != nil {
-        return nil, err
-    }
+	dsn := "root:superroot@tcp(db:3306)/jobsearchdb?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
 
-    return db, nil
+	return db, nil
 }
 
-
 func (s MariaDBStore) Add(id int, application JobApplication) error {
+	application.Id = 0
 	result := s.db.Create(&application)
-    if result.Error != nil {
-        return result.Error
-    }
-    return nil
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
 
 }
 
@@ -55,9 +54,9 @@ func (s MariaDBStore) Get(Id int) (*JobApplication, error) {
 		return nil, result.Error
 	}
 	return &application, nil
-} 
+}
 
-func (s MariaDBStore) List() ([]JobApplication, error){
+func (s MariaDBStore) List() ([]JobApplication, error) {
 	var applications []JobApplication
 	s.db.Find(&applications)
 	return applications, nil
@@ -72,8 +71,7 @@ func (s MariaDBStore) Update(id int, application JobApplication) error {
 }
 
 func (s MariaDBStore) Remove(id int) error {
-	
-	
+
 	application, err := s.Get(id)
 	if err != nil {
 		return err
@@ -83,7 +81,5 @@ func (s MariaDBStore) Remove(id int) error {
 		return result.Error
 	}
 	return nil
-	
+
 }
-
-
