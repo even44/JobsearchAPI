c8f59300d06b4ff91b151e021795763c283d1c47
even44 <even.andreas.ingeberg@ebnett.no>
2025-01-26 14:08:04 +0100

    updated models to use time.Time istead of string

--- pkg/models/jobApplicationModel.go
+++ pkg/models/jobApplicationModel.go
@@ -1,16 +1,20 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type JobApplication struct {
 	gorm.Model
-	Position   string  `json:"position"`
-	CompanyId  int     `json:"company_id"`
-	Company    Company `json:"company"`
-	SearchDate string  `json:"search_date"`
-	Deadline   string  `json:"deadline"`
-	Response   bool    `json:"response"`
-	Interview  bool    `json:"interview"`
-	Done       bool    `json:"done"`
-	Link       string  `json:"link"`
+	Position   string    `json:"position"`
+	CompanyId  int       `json:"company_id"`
+	Company    Company   `json:"company"`
+	SearchDate time.Time `json:"search_date"`
+	Deadline   time.Time `json:"deadline"`
+	Response   bool      `json:"response"`
+	Interview  bool      `json:"interview"`
+	Done       bool      `json:"done"`
+	Link       string    `json:"link"`
 }
