05eec1e051e8ec7309b1e3110d7f73a598106e33
even44 <even.andreas.ingeberg@ebnett.no>
2025-01-26 21:36:53 +0100

    hotfix 2

--- pkg/stores/jobApplicationMariaDBStore.go
+++ pkg/stores/jobApplicationMariaDBStore.go
@@ -52,7 +52,7 @@ func (s MariaDBStore) AddApplication(application models.JobApplication) (*models
 }
 func (s MariaDBStore) GetApplication(Id uint) (*models.JobApplication, error) {
 	var application models.JobApplication
-	result := s.db.Preload("Company.Contacts").Preload("Company").First(&application, Id)
+	result := s.db.Preload("Contact").Preload("Company.Contacts").Preload("Company").First(&application, Id)
 	if result.Error != nil {
 		return nil, result.Error
 	}
@@ -61,7 +61,7 @@ func (s MariaDBStore) GetApplication(Id uint) (*models.JobApplication, error) {
 }
 func (s MariaDBStore) ListApplications(userID uint) ([]models.JobApplication, error) {
 	var applications []models.JobApplication
-	s.db.Preload("Company.Contacts").Preload("Company").Find(&applications, &models.JobApplication{UserID: userID})
+	s.db.Preload("Contact").Preload("Company.Contacts").Preload("Company").Find(&applications, &models.JobApplication{UserID: userID})
 	s.logger.Printf("[LIST] Got %d job applications", len(applications))
 	return applications, nil
 }
