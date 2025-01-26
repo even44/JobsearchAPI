a28b867bb4fee92a9e4c175df7124a0a58a3f923
even44 <even.andreas.ingeberg@ebnett.no>
2025-01-26 16:57:50 +0100

    formatting

--- pkg/middleware/logging.go
+++ pkg/middleware/logging.go
@@ -13,6 +13,6 @@ func Logging(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 		next.ServeHTTP(w, r)
-		logLogger.Printf("%s %s %s\n", r.Method, r.URL.Path, time.Since(start))
+		logLogger.Printf("%s %s %s\n\n", r.Method, r.URL.Path, time.Since(start))
 	})
 }
