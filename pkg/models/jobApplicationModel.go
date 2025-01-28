df395396eac49552dcdd40417263b6a794dca31e
Even <even.andreas.ingeberg@ebnett.no>
2025-01-28 11:39:57 +0100

    removed gorm.model

--- pkg/models/jobApplicationModel.go
+++ pkg/models/jobApplicationModel.go
@@ -1,19 +1,13 @@
 package models
 
-import (
-	"time"
-
-	"gorm.io/gorm"
-)
-
 type JobApplication struct {
-	gorm.Model
+	ID         uint      `gorm:"primarykey"`
 	UserID     uint      `json:"user_id" gorm:"index:idx_application"`
 	CompanyID  uint      `json:"company_id" gorm:"index:idx_application"`
 	Position   string    `json:"position"`
 	Company    Company   `json:"company"`
-	SearchDate time.Time `json:"search_date"`
-	Deadline   time.Time `json:"deadline"`
+	SearchDate CivilTime `json:"search_date"`
+	Deadline   CivilTime `json:"deadline"`
 	Response   bool      `json:"response"`
 	Interview  bool      `json:"interview"`
 	Done       bool      `json:"done"`
